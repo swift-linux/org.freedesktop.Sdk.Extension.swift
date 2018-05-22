a9e25878428ca006a081de07f60af71240f71e3d
Ryan Gonzalez <rymg19@gmail.com>
2018-05-22 12:34:01 -0500

    Move over to YAML

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ repo="$base/repo"
 
 which="$1"
 action="$2"
-shift 2
 
 GPG_KEY="${GPG_KEY:-3D0FA8117F28EEE7FEAA305D6B2F86B16910DDA0}"
 
@@ -18,9 +17,9 @@ run() {
 }
 
 case "$which" in
-sdk) file="$base/org.freedesktop.Sdk.Extension.swift4.json"; root="$base/root" ;;
-live) file="$base/org.freedesktop.Sdk.Extension.swift4.live.json"; root="$base/root.live" ;;
-example) file="$base/org.freedesktop.Sdk.Extension.swift4.example.json"; root="$base/root.ex" ;;
+sdk) file="$base/org.freedesktop.Sdk.Extension.swift4.yaml"; root="$base/root" ;;
+live) file="$base/org.freedesktop.Sdk.Extension.swift4.live.yaml"; root="$base/root.live" ;;
+example) file="$base/org.freedesktop.Sdk.Extension.swift4.example.yaml"; root="$base/root.ex" ;;
 remote-add) run flatpak --user remote-add --no-gpg-verify swift-local "file://$base/repo"; exit ;;
 remote-delete) run flatpak remote-delete swift-local; exit ;;
 clear-cache) rm -rf "$base/.flatpak-builder/build"; exit ;;
@@ -36,12 +35,13 @@ build) args=--force-clean ;;
 run) args=--run ;;
 repo) args="--repo=$base/repo --force-clean" ;;
 pub) args="--repo=$base/pub --force-clean" ;;
-install) run flatpak --user install swift-local `basename "$file" .json`; exit ;;
-uninstall) run flatpak --user uninstall `basename "$file" .json`; exit ;;
-update) run flatpak --user update `basename "$file" .json`; exit ;;
+install) run flatpak --user install swift-local `basename "$file" .yaml`; exit ;;
+uninstall) run flatpak --user uninstall `basename "$file" .yaml`; exit ;;
+update) run flatpak --user update `basename "$file" .yaml`; exit ;;
 *) echo 'Invalid argument.'; exit 1 ;;
 esac
 
+shift 2
 run flatpak-builder $args "$root" "$file" "$@"
 
 if [ "$action" = "pub" ]; then
